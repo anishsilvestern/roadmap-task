3ab10d4d6ae3db796988f3dc2b35f351057d18bb
anishsilvestern <neooanish@gmail.com>
2024-02-03 16:51:09 +0530

    day3--task--added-2

--- day3/Q1-answer.js
+++ day3/Q1-answer.js
@@ -24,7 +24,7 @@ function Equal(obj1, obj2) {
 }
 
 let obj1 = { name: "person 1", age: 5};
-let obj2 = { age: 51, name: "person 1"};
+let obj2 = { age: 5, name: "person 1"};
 
 let areEqual = Equal(obj1, obj2);
 
