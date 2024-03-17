4886bf17f955a70d9473f9f2c13bed67536a8f72
anishsilvestern <neooanish@gmail.com>
2024-03-17 22:54:25 +0530

    photo visibilty check in netlify-5

--- src/App.jsx
+++ src/App.jsx
@@ -13,55 +13,55 @@ const Product =  {
    details: 
    [
        {
-           photo:'',
+           photo:<img src= './src/assets/Screenshot Capture - 2024-03-14 - 13-24-40.jpg' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Camera Photo"></img>,
            rating: '★★★★☆',
            name: 'Camera',
            Price: '₹1,50,000 - ₹2,50,000'
        },
    
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 14-16-10.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-16-10.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Perfume Photo"></img>,
            rating: '★★★☆☆',
            name: 'Prefume',
            Price: '₹249 - ₹349'
        },
 
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 14-28-15.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-28-15.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Phone Photo"></img>,
            rating: '★★★★★',
            name: 'Mobile Phone',
            Price: '₹49,999 - ₹59,999'
        },
 
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 14-49-31.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-49-31.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Shoe Photo"></img>,
            rating: '★★★★☆',
            name: 'Running Shoe',
            Price: '₹1,499 - ₹2,499'
        },
 
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 14-52-29.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-52-29.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Oven Photo"></img>,
            rating: '★★★☆☆',
            name: 'Microwave Oven',
            Price: '₹7,499 - ₹9,499'
        },
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 14-54-38.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-54-38.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Trolley Photo"></img>,
            rating: '★★★★★',
            name: 'Trolley Bags',
            Price: '₹1,499 - ₹2,499'
        },
 
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 14-59-12.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-59-12.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="TV Photo"></img>,
            rating: '★★★★☆',
            name: 'LED Television',
            Price: '₹74,999 - ₹84,999'
        },
 
        {
-           photo: './assets/Screenshot Capture - 2024-03-14 - 15-03-57.png',
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 15-03-57.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Watch Photo"></img>,
            rating: '★☆☆☆☆',
            name: 'Smart Watch',
            Price: '₹1,499 - ₹1,999'
