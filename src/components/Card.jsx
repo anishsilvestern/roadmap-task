553be25bd8d452e9eac3f1b771a0d5425103000d
anishsilvestern <neooanish@gmail.com>
2024-03-17 21:47:43 +0530

    photo visibilty check in netlify-4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const Card = ({productdetails, addToCart , removeFromCart}) => {
     return (
     <div className="col-lg-3" >   
         <div className="card container-fluid   " style={{width: "18rem"}}>
-                 {productdetails.photo}  
+        <img src= './src/assets/Screenshot Capture - 2024-03-14 - 13-24-40.jpg' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Product Photo"></img>  
             <div className="card-body text-center">
                 <h3 className="card-title mt-4">{productdetails.name}</h3>
                 <h5 className="card-title mt-3">{productdetails.rating}</h5>
