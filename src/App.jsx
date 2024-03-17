cc2528ec5000d8e389acfa6ed38face1d8819704
anishsilvestern <neooanish@gmail.com>
2024-03-17 23:02:55 +0530

    photo visibilty check in netlify-6

--- src/App.jsx
+++ src/App.jsx
@@ -13,55 +13,55 @@ const Product =  {
    details: 
    [
        {
-           photo:<img src= './src/assets/Screenshot Capture - 2024-03-14 - 13-24-40.jpg' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Camera Photo"></img>,
+           photo:<img src= './src/assets/Screenshot Capture - 2024-03-14 - 13-24-40.jpg' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Camera Photo"/>,
            rating: '★★★★☆',
            name: 'Camera',
            Price: '₹1,50,000 - ₹2,50,000'
        },
    
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-16-10.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Perfume Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-16-10.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Perfume Photo"/>,
            rating: '★★★☆☆',
            name: 'Prefume',
            Price: '₹249 - ₹349'
        },
 
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-28-15.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Phone Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-28-15.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Phone Photo"/>,
            rating: '★★★★★',
            name: 'Mobile Phone',
            Price: '₹49,999 - ₹59,999'
        },
 
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-49-31.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Shoe Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-49-31.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Shoe Photo"/>,
            rating: '★★★★☆',
            name: 'Running Shoe',
            Price: '₹1,499 - ₹2,499'
        },
 
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-52-29.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Oven Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-52-29.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Oven Photo"/>,
            rating: '★★★☆☆',
            name: 'Microwave Oven',
            Price: '₹7,499 - ₹9,499'
        },
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-54-38.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Trolley Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-54-38.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Trolley Photo"/>,
            rating: '★★★★★',
            name: 'Trolley Bags',
            Price: '₹1,499 - ₹2,499'
        },
 
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-59-12.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="TV Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 14-59-12.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="TV Photo"/>,
            rating: '★★★★☆',
            name: 'LED Television',
            Price: '₹74,999 - ₹84,999'
        },
 
        {
-           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 15-03-57.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Watch Photo"></img>,
+           photo: <img src= './src/assets/Screenshot Capture - 2024-03-14 - 15-03-57.png' className="card-img-top mt-2" style={{borderRadius: '10px'}} alt="Watch Photo"/>,
            rating: '★☆☆☆☆',
            name: 'Smart Watch',
            Price: '₹1,499 - ₹1,999'
