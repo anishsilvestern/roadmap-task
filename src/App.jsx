0f8eb0981b5e412d220c08115fa7d5966f9f80b5
anishsilvestern <neooanish@gmail.com>
2024-03-17 21:28:22 +0530

    again changes in assets floder

--- src/App.jsx
+++ src/App.jsx
@@ -13,55 +13,55 @@ const Product =  {
    details: 
    [
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 13-24-40.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 13-24-40.png',
            rating: '★★★★☆',
            name: 'Camera',
            Price: '₹1,50,000 - ₹2,50,000'
        },
    
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 14-16-10.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 14-16-10.png',
            rating: '★★★☆☆',
            name: 'Prefume',
            Price: '₹249 - ₹349'
        },
 
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 14-28-15.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 14-28-15.png',
            rating: '★★★★★',
            name: 'Mobile Phone',
            Price: '₹49,999 - ₹59,999'
        },
 
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 14-49-31.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 14-49-31.png',
            rating: '★★★★☆',
            name: 'Running Shoe',
            Price: '₹1,499 - ₹2,499'
        },
 
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 14-52-29.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 14-52-29.png',
            rating: '★★★☆☆',
            name: 'Microwave Oven',
            Price: '₹7,499 - ₹9,499'
        },
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 14-54-38.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 14-54-38.png',
            rating: '★★★★★',
            name: 'Trolley Bags',
            Price: '₹1,499 - ₹2,499'
        },
 
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 14-59-12.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 14-59-12.png',
            rating: '★★★★☆',
            name: 'LED Television',
            Price: '₹74,999 - ₹84,999'
        },
 
        {
-           photo: './src/assets/Screenshot Capture - 2024-03-14 - 15-03-57.png',
+           photo: './assets/Screenshot Capture - 2024-03-14 - 15-03-57.png',
            rating: '★☆☆☆☆',
            name: 'Smart Watch',
            Price: '₹1,499 - ₹1,999'
